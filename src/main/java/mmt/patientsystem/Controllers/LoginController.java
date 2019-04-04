8f07919077403e719b195e2f2f285a6338535799
MarkusDrejer <mabd412b@stud.kea.dk>
2019-04-04 12:28:48 +0200

    Small changes

--- src/main/java/mmt/patientsystem/Controllers/LoginController.java
+++ src/main/java/mmt/patientsystem/Controllers/LoginController.java
@@ -23,11 +23,6 @@ public class LoginController {
         return "index";
     }
 
-    @GetMapping("/home")
-    public String homePage(){
-        return "home";
-    }
-
     @PostMapping("/login")
     public String login(HttpSession httpSession, @ModelAttribute User user, Model model) throws SQLException {
 
