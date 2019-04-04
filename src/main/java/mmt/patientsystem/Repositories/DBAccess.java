48f56a20bef0b2b45bbc3b0be4b45737f8fef83f
MarkusDrejer <mabd412b@stud.kea.dk>
2019-04-04 23:09:24 +0200

    Slight rework to DBAccess to try for better connection setup, but no implementation in other repositories

--- src/main/java/mmt/patientsystem/Repositories/DBAccess.java
+++ src/main/java/mmt/patientsystem/Repositories/DBAccess.java
@@ -9,17 +9,20 @@ import java.sql.SQLException;
 @Repository
 public class DBAccess {
 
-    public Connection getConnection() {
-        Connection connection = null;
+    private Connection connection;
+
+    public DBAccess() {
         try {
             connection = DriverManager.getConnection(
                     "jdbc:mysql://den1.mysql6.gear.host/patientsystemdb",
                     "patientsystemdb",
                     "Hn5Y-xGfN-8W");
-            return connection;
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+    }
+
+    public Connection getConnection() {
+        return connection;
     }
 }
